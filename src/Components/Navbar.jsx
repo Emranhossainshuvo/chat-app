c245d042d3a2de12fd661d0da79a81c61def1dc3
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-11 19:52:15 +0600

    added navbar and some contents in the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,15 @@
 
 const Navbar = () => {
     return (
-        <div>
-           <span className="tracking-tighter">
-            Talks chat
-           </span>
+        <div className="flex items-center bg-[#2e2d54] h-[50px] p-3 justify-between text-white">
+            <span className="tracking-tighter font-bold">
+                Talks chat
+            </span>
+            <div className="flex gap-10">
+                <img className="bg-white h-6 w-6 rounded-[50%] object-cover" src="https://i.ibb.co/DQR0TGc/126.jpg" alt="" />
+                <span>John</span>
+                <button className="bg-purple-800 text-gray-200 text-xs rounded-md border-none p-2">Log out</button>
+            </div>
         </div>
     );
 };
