e6659f2b55f1c9b4c0cc5db38a4298738556ec85
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-11 21:50:39 +0600

    added some contents and some styles in the search page

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,16 @@
 
 const Search = () => {
     return (
-        <div>
-            
+        <div className="border-b-2 searchhh border-gray-400">
+            <div className="serchform">
+                <input className="bg-transparent border-none text-white outline-none" placeholder="Find a user " type="text" />
+            </div>
+            <div className="userchat p-3 flex items-center">
+                <img className="w-[50px] h-[50px] rounded-[50%] object-cover" src="https://i.ibb.co/DQR0TGc/126.jpg" alt="" />
+                <div>
+                    <span>Jane</span>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import Search from "./Search";
 
 const Sidebar = () => {
     return (
         <div className="flex-1  bg-[#c4dfe6]">
                 <Navbar />
+                <Search />
         </div>
     );
 };
