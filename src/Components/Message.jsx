3497d5a8deb82d30b595127eeba81669f749cb11
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-12 19:49:10 +0600

    added and styled contents in the message file and installed scss to styles

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,14 +1,14 @@
 
 const Message = () => {
     return (
-        <div className="messege">
-            <div className="messageInfo">
-                <img src="https://i.ibb.co/tCKkPkt/2147956506.jpg" alt="" />
+        <div className="messege owner mb-[20px] flex gap-[20px]">
+            <div className="messageInfo flex flex-col text-gray-600 font-normal">
+                <img className="w-[40px] h-[40px] rounded-[50%] object-cover" src="https://i.ibb.co/tCKkPkt/2147956506.jpg" alt="" />
                 <span>Just now</span>
             </div>
-            <div className="messageContent">
-                <p>Hello</p>
-                <img src="https://i.ibb.co/tCKkPkt/2147956506.jpg" alt="" />
+            <div className="messageContent max-w-[80%] flex flex-col gap-2">
+                <p className="bg-white p-5 rounded-tl-none rounded-tr-lg rounded-br-lg rounded-bl-lg">Hello</p>
+                {/* <img className="w-[50%]" src="https://i.ibb.co/tCKkPkt/2147956506.jpg" alt="" /> */}
             </div>
         </div>
     );
--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -2,7 +2,7 @@ import Message from "./Message";
 
 const Messages = () => {
     return (
-        <div className="messages bg-slate-300 p-[10px]" style={{height: "calc(100% - 100px)"}}>
+        <div className="messages overflow-scroll bg-slate-300 p-[10px]" style={{height: "calc(100% - 100px)"}}>
             <Message />
             <Message />
             <Message />
