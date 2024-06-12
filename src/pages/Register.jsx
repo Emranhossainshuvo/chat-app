e26ec4cb4836ae6993c8c7261872fbe1fa2875dd
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-12 23:55:01 +0600

    firebase authentication register done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,32 @@
+import {  createUserWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
+import {auth} from "../firebase"
 
 const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('cllicke')
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+
     }
 
     return (
