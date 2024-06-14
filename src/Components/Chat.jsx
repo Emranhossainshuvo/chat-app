598473b488ea452d0202b16b62bd019966bd83b0
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-14 22:26:22 +0600

    dynamically name chandeged to the current chat name in the home navbar

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import Input from "./Input";
 import Messages from "./Messages";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+
+    const { data } = useContext(ChatContext);
+
     return ( 
         <div className="chat " style={{flex: 2}}>
             <div className="chatInfo justify-between p-[10px] text-gray-400 flex h-[50px] bg-[#5d597f] items-center">
-                <span>Jane</span>
+            <span>{data.user?.displayName}</span>
                 <div className="chatIcons flex gap-5">
                     <img className="h-[24px] cursor-pointer" src="https://i.ibb.co/WHPJQvB/icons8-video-camera-24-1.png" alt="" />
                     <img className="h-[24px] cursor-pointer" src="https://i.ibb.co/m54zN7r/icons8-add-friend-24.png" alt="" />
